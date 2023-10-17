a22e55e29a0e1cc1fc965e73ed0aa65126eff039
Gitanshu <lusifer0418@gmail.com>
2023-10-17 22:10:19 +0530

    create app structure

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
+//  modules and libraries
 const express = require('express');
+const bodyParser = require('body-parser');
+const Seq = require('sequelize');
+require('dotenv').config();
+
+
+// custom modules
+const routes = require('./routes');
+const seqEnv = require('./database');
+
 
 const app = express();
-const port = 3000;
 
-app.listen(port, ()=>{
-    console.log(`server is listening at port: http://localhost:${port}`)
+const port = process.env.port;
+
+// configuring app settings
+app.use(bodyParser.urlencoded({extended: false})); // setting bodyparser to encode incoming data 
+app.set('view engine', 'ejs'); // setting view engine to ejs
+app.set('views', 'views'); // setting default views directory 
+
+app.use(routes);
+
+
+// syncing sequelize connection to database
+seqEnv.sync().then((res)=>{
+    console.log(res);
+    // listening to server if connection to database is succesfull
+    app.listen(port, ()=>{
+        console.log(`server is listening at port: http://localhost:${port}`);
+    })
+}).catch(err =>{
+    console.log(err);
 })
+
