b0b81f644f12a5acdf36c7ea71e564cdbbec7b47
Gitanshu <lusifer0418@gmail.com>
2023-10-18 21:38:33 +0530

    add multer and new routes

--- routes.js
+++ routes.js
@@ -1,5 +1,18 @@
 //  importing modules and libraries
 const express = require('express');
+const multer = require('multer');
+
+// Storage Engine
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+       return cb(null, 'uploads');
+    },
+    filename: function (req, file, cb)  {
+       return cb(null, `${Date.now()}#${file.originalname}`)
+    }
+  })
+
+const upload = multer({ storage });
 
 // controller files
 const controller = require('./controller');
@@ -9,6 +22,11 @@ const router = express.Router();
 
 // getting incoming request and calling proper controller logic 
 router.get('/', controller.getIndex);
+router.get('/add-product', controller.getForm);
+router.post('/admin/add-product', upload.single('image'), controller.postAddProduct);
+router.get('/admin/edit-product/:productId', controller.getEditProduct);
+router.post('/admin/edit-product/:productId', upload.single('image'), controller.postEditProduct);
+router.post("/admin/delete",  controller.postDeleteProduct);
 
 
 module.exports = router;
