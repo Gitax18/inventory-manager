2ecacdf3fdc766393cbbf27b7f5c7afc56b8b863
Gitanshu <lusifer0418@gmail.com>
2023-10-19 22:04:15 +0530

    Write routes for categories

--- routes.js
+++ routes.js
@@ -22,12 +22,22 @@ const router = express.Router();
 
 // getting incoming request and calling proper controller logic 
 router.get('/', controller.getIndex);
+// Product CRUD Routes
 router.get('/add-product', controller.getForm);
 router.get('/details/:productId', controller.getDetails);
 router.post('/admin/add-product', upload.single('image'), controller.postAddProduct);
-router.get('/admin/edit-product/:productId', controller.getEditProduct);
+router.get('/admin/edit-product/:productId', upload.single('image'), controller.getEditProduct);
 router.post('/admin/edit-product/:productId', upload.single('image'), controller.postEditProduct);
 router.post("/admin/delete",  controller.postDeleteProduct);
+
+// Category CRUD Routes
+router.get('/show-categories', controller.getCategories);
+router.get('/add-category', controller.getAddCategory);
+router.post('/admin/add-category', controller.postAddCategory);
+router.get('/admin/edit-category/:categoryId', controller.getEditCategory);
+router.post('/admin/edit-category/:categoryId', controller.postEditCategory)
+router.post('/admin/delete-category', controller.postDeleteCategory)
+
 
 
 module.exports = router;
